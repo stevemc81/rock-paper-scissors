3fa564ee27bf1373f1445cb7a054750657cbb214
Steven McWee <stevenmcwee@gmail.com>
2025-05-28 13:46:10 +0100

    Return rock, paper or scissors randomly in the console

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return 'Rock';
+    } else if (choice === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors'
+    };
+};
+
+console.log(getComputerChoice());
